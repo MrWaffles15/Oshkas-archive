125cb94127609659b335f1da78a72ad715e8a17b
MrWaffles15 <163484569+MrWaffles15@users.noreply.github.com>
2026-05-28 11:27:32 -0700

    initial commit

---
+++ Copy of script.js
@@ -0,0 +1,16 @@
+function buttonone(){
+  document.getElementById("mainstuff1").style.display = "block";
+  document.getElementById("mainstuff2").style.display = "none";
+  document.getElementById("mainstuff3").style.display = "none";
+}
+function buttontwo(){
+  document.getElementById("mainstuff2").style.display = "block";
+  document.getElementById("mainstuff1").style.display = "none";
+  document.getElementById("mainstuff3").style.display = "none";
+}
+function buttonthree(){
+  document.getElementById("mainstuff3").style.display = "block";
+  document.getElementById("mainstuff2").style.display = "none";
+  document.getElementById("mainstuff1").style.display = "none";
+}
+
